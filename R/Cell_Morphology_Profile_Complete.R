f9a2821bf63fc0c549a57d01febd03902ec97afc
isarnassiri <isar_nassiri@urmc.rochester.edu>
2017-06-12 12:27:02 -0400

    Delete Cell_Morphology_Profile_Complete.R

--- R/Cell_Morphology_Profile_Complete.R
+++
@@ -1,5 +0,0 @@
-#'@title{Single cell morphological profile}
-#'@description{
-#'The single cell morphological profiles of human bone osteosarcoma cells (U-2 OS cells) in response to the drugs or small molecule compounds from the library of network-based cellular signatures (LINCS) database (http://lincscloud.org). 
-#'}
-"Cell_Morphology_Profile_Complete"
