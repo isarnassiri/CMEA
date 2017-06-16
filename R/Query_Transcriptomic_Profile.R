a514ab95342c1c9a47e05630a32c6a6260181e43
isarnassiri <isar_nassiri@urmc.rochester.edu>
2017-06-16 15:35:09 -0400

    Revised.

---
+++ R/Query_Transcriptomic_Profile.R
@@ -0,0 +1,10 @@
+#'@title{Query Transcriptomic Profile}
+#'@description{
+#'This file contains a vector of landmark gene expression level in response to the treatment with a therapeutic agent.
+#'The sample input includes transcriptomic profile of U-2 OS cells in response to the treatment with ETOPOSIDE (BRD-K37798499). 
+#'ETOPOSIDE is considered an effective agent in combined chemotherapy of osteosarcoma patients.
+#'}
+#'@references{
+#'1. Gill, J., Ahluwalia, M.K., Geller, D. and Gorlick, R. (2013) New targets and approaches in osteosarcoma. Pharmacology & Therapeutics, 137, 89-99.
+#'}
+"Query_Transcriptomic_Profile"
